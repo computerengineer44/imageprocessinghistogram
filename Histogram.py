834878f22c133790b7c0675e966e85f991e302f1
Buğra Enhar <75737540+computerengineer44@users.noreply.github.com>
2022-10-26 21:48:39 +0300

    Update Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# resim okuma
+# reading image
 im = cv2.imread('resmim.jpg')
 cv2.imshow('resmim.jpg',im)
 cv2.waitKey(0)
-# RGB kanallarından ortalama değeri hesaplayın ve 1D dizisine düzleştirin
+# Calculate average value from RGB channels and flatten to 1D array
 vals = im.mean(axis=2).flatten()
-# Histogram hesaplama
+# Histogram calculation
 counts, bins = np.histogram(vals, range(257))
-# 0..255 değerlerine odaklanan grafik histogramı
+# Graph histogram focusing on the values ​​0..255
 plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
 plt.xlim([-0.5, 255.5])
 plt.show()
